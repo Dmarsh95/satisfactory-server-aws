e2db48bff06e3f3d49e89831a34bcf4c9f92d000
Dmarsh95 <daltonmarsh@gmail.com>
2025-11-28 08:20:41 -0600

    Update server-hosting.ts

--- server-hosting/server-hosting.ts
+++ server-hosting/server-hosting.ts
@@ -12,7 +12,7 @@ new ServerHostingStack(app, 'ServerHostingStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  // env: { account: process.env.143670579917, region: process.env.region: us-east-2 },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
