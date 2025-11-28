1495de56c34e058b22f2b9744b664db7aad24180
Dmarsh95 <daltonmarsh@gmail.com>
2025-11-28 10:45:25 -0600

    Update server-hosting.ts

--- server-hosting/server-hosting.ts
+++ server-hosting/server-hosting.ts
@@ -16,7 +16,7 @@ env: { account: process.env.143670579917, region: process.env.region: us-east-2}
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-env: { account: Config.account, region: Config.region},
+//env: { account: Config.account, region: Config.region},
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
