9cb5a14e27c3aa7792a2eeb8387e13dc9d804a2c
Dmarsh95 <daltonmarsh@gmail.com>
2025-11-28 08:52:16 -0600

    change

--- server-hosting/server-hosting.ts
+++ server-hosting/server-hosting.ts
@@ -12,11 +12,44 @@ new ServerHostingStack(app, 'ServerHostingStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.143670579917, region: process.env.region: us-east-2 },
+env: { account: process.env.143670579917, region: process.env.region: us-east-2 },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: Config.account, region: Config.region},
+env: { account: Config.account, region: Config.region},
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+export const Config = {
+     // compulsory parameters
+
+     // server hosting region
+     region: 'us-east-2',
+     // server hosting account
+     account: '143670579917',
+     // prefix for all resources in this app
+     prefix: 'SatisfactoryHosting',
+     // set to false if you don't want an api to
+     // restart game server and true if you do
+     restartApi: true,
+     // Set to true if you want to use Satisfactory Experimental
+     useExperimentalBuild: false,
+
+     // optional parameters
+
+     // bucket for storing save files
+     // you can use an existing bucket
+     // or leave it empty to create a new one
+     bucketName: '',
+     // server hosting vpc
+     // Create a vpc and it's id here
+     // or leave it empty to use default vpc
+     vpcId: '',
+     // specify server subnet
+     // leave blank (preferred option) for auto-placement
+     // If vpc is given specify subnet for that vpc
+     // If vpc is not given specify subnet for default vpc
+     subnetId: '',
+     // Needed if subnetId is specified (i.e. us-west-2a)
+     availabilityZone: ''
+};
